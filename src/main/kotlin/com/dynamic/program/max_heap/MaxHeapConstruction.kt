eb692b17d9c28049f6f06035d2c782b6c442d0c1
Arun Kumar <reddyarunkumar@gmail.com>
2022-02-27 11:45:40 -0700

    more code!!

--- src/main/kotlin/com/dynamic/program/max_heap/MaxHeapConstruction.kt
+++ src/main/kotlin/com/dynamic/program/max_heap/MaxHeapConstruction.kt
@@ -1,41 +1,9 @@
-package com.dynamic.program.min_heap
+package com.dynamic.program.max_heap
 
-/**
- * Implement a <span>MinHeap class that supports:
- *
- * Building a Min Heap from an input array of integers.
- *  Inserting integers in the heap.
- *  Removing the heap's minimum / root value.
- *  Peeking at the heap's minimum / root value.
- *
- *  Sifting integers up and down the heap, which is to be used when inserting and removing values.
- *
- * Note that the heap should be represented in the form of an array.
- *
- * If you're unfamiliar with Min Heaps, we recommend watching the
- * Conceptual Overview section of this question's video explanation before
- * starting to code.
- *
- * Sample Usage
- * array = [48, 12, 24, 7, 8, -5, 24, 391, 24, 56, 2, 6, 8, 41]
- *
- * // All operations below are performed sequentially.
- * MinHeap(array): - // instantiate a MinHeap (calls the buildHeap method and populates the heap)
- * buildHeap(array): - [-5, 2, 6, 7, 8, 8, 24, 391, 24, 56, 12, 24, 48, 41]
- * insert(76): - [-5, 2, 6, 7, 8, 8, 24, 391, 24, 56, 12, 24, 48, 41, 76]
- * peek(): -5
- * remove(): -5 [2, 7, 6, 24, 8, 8, 24, 391, 76, 56, 12, 24, 48, 41]
- * peek(): 2
- * remove(): 2 [6, 7, 8, 24, 8, 24, 24, 391, 76, 56, 12, 41, 48]
- * peek(): 6
- * insert(87): - [6, 7, 8, 24, 8, 24, 24, 391, 76, 56, 12, 41, 48, 87]
-
-
- */
 // Do not edit the class below except for the buildHeap,
 // siftDown, siftUp, peek, remove, and insert methods.
 // Feel free to add new properties and methods to the class.
-open class MinHeap(array: MutableList<Int>) {
+open class MaxHeap(array: MutableList<Int>) {
     val heap = this.buildHeap(array)
 
     fun buildHeap(array: MutableList<Int>): MutableList<Int> {
@@ -53,13 +21,13 @@ open class MinHeap(array: MutableList<Int>) {
         var childIdx = currentIdx * 2 + 1
         while (childIdx <= endIdx) {
             val childIdxTwo = if (newCurrentIdx * 2 + 2 <= endIdx) newCurrentIdx * 2 + 2 else -1
-            val idxSwap: Int = if (childIdxTwo != -1 && heap[childIdxTwo] < heap[childIdx]) {
+            val idxSwap: Int = if (childIdxTwo != -1 && heap[childIdxTwo] > heap[childIdx]) {
                 childIdxTwo
             } else {
                 childIdx
             }
 
-            if (heap[idxSwap] < heap[newCurrentIdx]) {
+            if (heap[idxSwap] > heap[newCurrentIdx]) {
                 swap(heap, newCurrentIdx, idxSwap)
                 newCurrentIdx = idxSwap
                 childIdx = newCurrentIdx * 2 + 1
@@ -73,7 +41,7 @@ open class MinHeap(array: MutableList<Int>) {
         // Write your code here.
         var currentIdx = cIdx
         var parentIdx = (currentIdx - 1) / 2
-        while (currentIdx > 0 && heap[currentIdx] < heap[parentIdx]) {
+        while (currentIdx > 0 && heap[currentIdx] > heap[parentIdx]) {
             swap(heap, currentIdx, parentIdx)
             currentIdx = parentIdx
             parentIdx = (currentIdx - 1) / 2
@@ -107,5 +75,5 @@ open class MinHeap(array: MutableList<Int>) {
 }
 
 fun main() {
-    println(MinHeap(mutableListOf(48, 12, 24, 7, 8, -5)).heap)
+    println(MaxHeap(mutableListOf(48, 12, 24, 7, 8, -5)).heap)
 }
