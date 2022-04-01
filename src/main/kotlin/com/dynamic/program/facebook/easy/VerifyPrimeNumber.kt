86fda86cbbe51447deafdd127b1b4a31daeff005
Arun Kumar <reddyarunkumar@gmail.com>
2022-03-31 19:23:51 -0600

    more code!!

--- src/main/kotlin/com/dynamic/program/facebook/easy/VerifyPrimeNumber.kt
+++ src/main/kotlin/com/dynamic/program/facebook/easy/VerifyPrimeNumber.kt
@@ -1,6 +1,7 @@
 package com.dynamic.program.facebook.easy
 
 import kotlin.math.round
+import kotlin.math.sqrt
 
 /**
  * Given a number N, verify if N is prime or not.
@@ -21,13 +22,13 @@ Complete code in the hint.
 fun verifyPrimeNumberBruteForce(v: Int): Boolean {
     if (v == 2 || v == 1) return true
     if (v%2 == 0) return false
-    return checkPrime(v, round(v/2.0).toInt())
+    return checkPrime(v, sqrt(v.toFloat()).toInt())
 }
 
 fun checkPrime(v: Int, divisible: Int): Boolean {
     if (divisible == 1) return true
     if (v%divisible == 0) return false
-    return checkPrime(v, round(divisible/2.0).toInt())
+    return checkPrime(v, divisible - 1)
 }
 
 fun findAllPrimeNumbers(v: Int): List<Int> {
@@ -41,6 +42,7 @@ fun findAllPrimeNumbers(v: Int): List<Int> {
 }
 
 fun main() {
-    println(verifyPrimeNumberBruteForce(99))
+    println(sqrt(21.0))
+    println(verifyPrimeNumberBruteForce(21))
     println(findAllPrimeNumbers(99))
 }
