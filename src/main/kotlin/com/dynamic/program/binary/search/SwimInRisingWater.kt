6320b9acd6a471b409df2db957e15662bc85b137
Arun Kumar <arun.kumar@swisslog-healthcare.com>
2023-01-25 09:24:40 -0700

    another work

---
+++ src/main/kotlin/com/dynamic/program/binary/search/SwimInRisingWater.kt
@@ -0,0 +1,86 @@
+package com.dynamic.program.binary.search
+
+import java.util.*
+
+
+/**
+ * Swim in Rising Water
+ * You are given an n x n integer matrix grid where each value grid[i][j] represents the elevation at that point (i, j).
+ * The rain starts to fall. At time t, the depth of the water everywhere is t.
+ * You can swim from a square to another 4-directionally adjacent square if and only if the elevation of both squares individually are at most t.
+ * You can swim infinite distances in zero time. Of course, you must stay within the boundaries of the grid during your swim.
+ * Return the least time until you can reach the bottom right square (n - 1, n - 1) if you start at the top left square (0, 0).
+ *
+ *
+ * Example 1:
+ * Input: grid = [[0,2],[1,3]]
+ * Output: 3
+ * Explanation:
+ * At time 0, you are in grid location (0, 0).
+ * You cannot go anywhere else because 4-directionally adjacent neighbors have a higher elevation than t = 0.
+ * You cannot reach point (1, 1) until time 3.
+ * When the depth of water is 3, we can swim anywhere inside the grid.
+ *
+ *
+ * Example 2:
+ * Input: grid = [[0,1,2,3,4],[24,23,22,21,5],[12,13,14,15,16],[11,17,18,19,20],[10,9,8,7,6]]
+ * Output: 16
+ * Explanation: The final route is shown.
+ * We need to wait until time 16 so that (0, 0) and (4, 4) are connected.
+ *
+ *
+ *
+ * Constraints:
+ * n == grid.length
+ * n == grid[i].length
+ * 1 <= n <= 50
+ * 0 <= grid[i][j] < n^2
+ * Each value grid[i][j] is unique.
+ *
+ */
+class SwimInRisingWater {
+
+    fun swimInWater(grid: Array<IntArray>): Int {
+        val dir = listOf(Pair(0, 1), Pair(1, 0), Pair(0, -1), Pair(-1, 0))
+        val m = grid.size
+        val n = grid[0].size
+
+        val visited = MutableList(m) { BooleanArray(n) }
+        visited[0][0] = true
+
+        var max = grid[0][0]
+
+        val q = PriorityQueue<Triple<Int, Int, Int>> { x, y -> x.first - y.first }
+        q.add(Triple(max, 0, 0))
+        while (q.isNotEmpty()) {
+            val (v, r, c) = q.remove()
+            max = Math.max(max, v)
+            if (r == m - 1 && c == n - 1) return max
+            dir.forEach { (r1, c1) ->
+                val nr = r + r1
+                val nc = c + c1
+                if (valid(nr, nc, grid, visited)) {
+                    q.add(Triple(grid[nr][nc], nr, nc))
+                    visited[nr][nc] = true
+                }
+            }
+        }
+        return max
+    }
+
+    fun valid(r: Int, c: Int, grid: Array<IntArray>, visited: MutableList<BooleanArray>): Boolean =
+        (r in grid.indices && c in grid[r].indices && !visited[r][c])
+}
+
+fun main() {
+    println(
+        SwimInRisingWater().swimInWater(
+            arrayOf(
+                intArrayOf(10, 12, 4, 6),
+                intArrayOf(9, 11, 3, 5),
+                intArrayOf(1, 7, 13, 8),
+                intArrayOf(2, 0, 15, 14)
+            )
+        )
+    )
+}
