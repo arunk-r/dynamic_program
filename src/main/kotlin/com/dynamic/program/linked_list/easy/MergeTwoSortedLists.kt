a5e0bcf55a288a4ef0f72b61c49feda020559416
Arun Kumar <arun.kumar@swisslog-healthcare.com>
2023-06-13 06:49:43 -0600

    another work

--- src/main/kotlin/com/dynamic/program/linked_list/easy/MergeTwoSortedLists.kt
+++ src/main/kotlin/com/dynamic/program/linked_list/easy/MergeTwoSortedLists.kt
@@ -40,7 +40,20 @@ Both list1 and list2 are sorted in non-decreasing order.
  */
 class MergeTwoSortedLists {
     data class ListNode(var `val`: Int, var next: ListNode? = null)
-    fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+    fun mergeTwoLists(l1: ListNode?, l2: ListNode?): ListNode? {
+        if (l1 == null && l2 == null) return null
+        else if (l1 == null) return l2
+        else if (l2 == null) return l1
+        else if (l1.`val` < l2.`val`) {
+            l1.next = mergeTwoLists(l1.next, l2)
+            return l1
+        } else {
+            l2.next = mergeTwoLists(l2.next, l1)
+            return l2
+        }
+    }
+
+    fun mergeTwoLists1(list1: ListNode?, list2: ListNode?): ListNode? {
         var node: ListNode? = null
         var root: ListNode? = null
 
